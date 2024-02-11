910d0620c83276ef7a57bcaeade22244e676058c
vova836 <123488703+vova836@users.noreply.github.com>
2024-02-11 19:13:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace П.Р2;
+
+public static class Program
+{
+	public static void Main() 
+{
+    Employee emp("Янишевский", "Владимир", 50000, 1.2);
+    emp.printInfo();
+    
+    Engineer eng("Томайлы", "Сергей", 60000, 1.5, 15);
+    eng.printInfo();
+    
+    Bus bus("Mercedes", 30, 100);
+    bus.printInfo();
+    
+    TouristBus touristBus("Volvo", 40, 80, 400);
+    touristBus.printInfo();
+    
+    return 0;
+}
+	}
+}
